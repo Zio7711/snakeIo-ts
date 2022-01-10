8f2e7869305b3ecb377e7807998d16b7ff474278
zio7711 <zi.ao.tan@outlook.com>
2022-01-10 13:42:43 +0800

    prevent from snake u turn

--- src/modules/Snake.ts
+++ src/modules/Snake.ts
@@ -25,6 +25,22 @@ class Snake {
       // 进入判断说明蛇撞墙了
       throw new Error('蛇撞墙了！');
     }
+
+    // 修改x时，是在修改水平坐标，蛇在左右移动，蛇在向左移动时，不能向右掉头，反之亦然
+    if (
+      this.bodies[1] &&
+      (this.bodies[1] as HTMLElement).offsetLeft === value
+    ) {
+      // 如果发生了掉头，让蛇向反方向继续移动
+      if (value > this.X) {
+        // 如果新值value大于旧值X，则说明蛇在向右走，此时发生掉头，应该使蛇继续向左走
+        value = this.X - 10;
+      } else {
+        // 向左走
+        value = this.X + 10;
+      }
+    }
+
     this.moveBody();
     this.head.style.left = value + 'px';
   }
@@ -35,6 +51,14 @@ class Snake {
     if (value < 0 || value > 290) {
       // 进入判断说明蛇撞墙了，抛出一个异常
       throw new Error('蛇撞墙了！');
+    }
+
+    if (this.bodies[1] && (this.bodies[1] as HTMLElement).offsetTop === value) {
+      if (value > this.Y) {
+        value = this.Y - 10;
+      } else {
+        value = this.Y + 10;
+      }
     }
 
     this.moveBody();
